03bb6ccf94ae2e0424ba9c91fedc8c7d6434cbe2
mmulyana <mulyan.t20@gmail.com>
2024-01-16 00:00:00 +0700

    feat: setup express, routes, and multer

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 
 const app = express()
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/image')
-  },
-  filename: (req, file, cb) => {
-    const fileType = file.mimetype.split('/')[1]
-    cb(null, `${Date.now()}.${fileType}`)
-  },
-})
+const multerStorage = multer.memoryStorage()
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith('image')) {
@@ -19,6 +12,20 @@ const multerFilter = (req, file, cb) => {
   } else {
     cb(new Error('Not an image'), false)
   }
+}
+
+const resizePhoto = (req, res, next) => {
+  if (!req.file) return next()
+
+  req.file.filename = `img-${Date.now()}.jpeg`
+
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/image/${req.file.filename}`)
+
+  next()
 }
 
 const upload = multer({
@@ -29,7 +36,7 @@ const upload = multer({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/upload', upload.single('photo'), async (req, res) => {
+app.post('/upload', upload.single('photo'), resizePhoto, async (req, res) => {
   try {
     console.log(req.file)
 
