1d7bbe5b6886f2c4d459587c694be60b478ff0c3
mmulyana <mulyan.t20@gmail.com>
2024-01-17 00:00:00 +0700

    feat: upload multiple images

--- server.js
+++ server.js
@@ -28,6 +28,26 @@ const resizePhoto = (req, res, next) => {
   next()
 }
 
+const resizeMultiplePhotos = async (req, res, next) => {
+  if (!req.files.images) return next()
+
+  req.body.images = []
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `${i+1}-${Date.now()}.jpeg`
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/image/${filename}`)
+      req.body.images.push(filename)
+    })
+  )
+
+  next()
+}
+
 const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
@@ -38,7 +58,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.post('/upload', upload.single('photo'), resizePhoto, async (req, res) => {
   try {
-    console.log(req.file)
+    console.log(req.body.images)
 
     res.status(200).json({ message: 'image upload success!' })
   } catch (error) {
@@ -46,6 +66,21 @@ app.post('/upload', upload.single('photo'), resizePhoto, async (req, res) => {
     res.status(400).json({ message: error.message })
   }
 })
+
+app.post(
+  '/multiple-upload',
+  upload.fields([{ name: 'images', maxCount: 5 }]),
+  resizeMultiplePhotos,
+  async (req, res) => {
+    try {
+      console.log(req.files)
+      res.status(200).json({ message: 'multiple image upload success' })
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ error: error.message })
+    }
+  }
+)
 
 app.listen(8000, () =>
   console.log('server is running in http://localhost:8000')
